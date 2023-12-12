dffd80fcf3ccce64cbe59735b2fd4fbf759b4aac
Benson <ibejibenson2@gmail.com>
2023-12-12 12:48:54 -0800

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints element of list
+ *
+ * @h: head of list
+ * Return: number of nodes in list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	if (h->next == NULL)
+	{
+		printf("%d", 0);
+		++nodes;
+		return (nodes);
+	}
+	else
+	{
+		do {
+		printf("%d\n", h->n);
+		h = h->next;
+		++nodes;
+		} while (h);
+	}
+	return (nodes);
+}
